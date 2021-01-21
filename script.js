dabd025991aa16efa3b5b9288c3ecb45d26ea7d0
Kelsey <BergstromKelsey@gmail.com>
2021-01-20 18:02:06 -0600

    first push

---
+++ script.js
@@ -0,0 +1,13 @@
+var contactbtn = $("#contact");
+var portfolibtn =$("#portfolio");
+var aboutbtn = $("#about");
+
+
+
+// portfolibtn.on('click', function () {
+//     window.location = "https://bergstromkelsey.github.io/responsiveness/portfolio";
+// })
+
+// aboutbtn.on('click', function () {
+//     window.location = "https://bergstromkelsey.github.io/responsiveness/#";
+// })
