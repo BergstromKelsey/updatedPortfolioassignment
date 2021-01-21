1c26ff8aed0079f6e4e483c1a2173d4348a9e064
Kelsey <BergstromKelsey@gmail.com>
2021-01-20 18:24:09 -0600

    third

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ var contactbtn = $("#contact");
 var portfolibtn =$("#portfolio");
 var aboutbtn = $("#about");
 
+portfolibtn.on('click', function () {
+    window.location = "contact.html";
+ })
 
 
  portfolibtn.on('click', function () {
-    window.location = "https://bergstromkelsey.github.io/updatedPortfolioassignment/portfolio.html";
+    window.location = "portfolio.html";
  })
 
  aboutbtn.on('click', function () {
